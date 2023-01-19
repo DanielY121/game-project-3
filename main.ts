9c8159b17f147e1a4de03f21e01f8ce6f33e2dbd
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-01-19 16:52:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,6 @@ input.onButtonPressed(Button.B, function () {
 let player: game.LedSprite = null
 player = game.createSprite(2, 4)
 let poison = game.createSprite(randint(0, 4), 0)
-let _777 = game.createSprite(randint(0, 4), 4)
-led.unplot(_777.get(LedSpriteProperty.X), _777.get(LedSpriteProperty.Y))
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         if (player == poison) {
