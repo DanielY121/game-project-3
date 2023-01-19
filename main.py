1b494658ce5264fb23fc68ffa4da0901dfc7f0e3
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-01-19 16:45:14 +0800

    found bug and remove bugged feature.

--- main.py
+++ main.py
@@ -1,57 +1,28 @@
-def on_gesture_tilt_left():
+def on_button_pressed_a():
     player.change(LedSpriteProperty.X, -1)
-input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_gesture_tilt_right():
+def on_button_pressed_b():
     player.change(LedSpriteProperty.X, 1)
-input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-poison2: game.LedSprite = None
-_this = 0
 player: game.LedSprite = None
 player = game.create_sprite(2, 4)
 poison = game.create_sprite(randint(0, 4), 0)
 
 def on_forever():
-    global _this, poison, poison2
-    _this = randint(1, 2)
-    if _this == 1:
-        for index in range(4):
-            if player == poison:
-                player.delete()
-                poison.delete()
-                game.game_over()
-            elif poison.get(LedSpriteProperty.Y) == 4:
-                poison.delete()
-                game.add_score(1)
-                poison = game.create_sprite(randint(0, 4), 0)
-            else:
-                basic.pause(500)
-                poison.change(LedSpriteProperty.Y, 1)
-    else:
-        for index2 in range(4):
-            if player == poison:
-                player.delete()
-                poison.delete()
-                poison2.delete()
-                game.game_over()
-            elif poison.get(LedSpriteProperty.Y) == 4:
-                poison.delete()
-                game.add_score(1)
-                poison = game.create_sprite(randint(0, 4), 0)
-            else:
-                basic.pause(500)
-                poison.change(LedSpriteProperty.Y, 1)
-        poison2 = game.create_sprite(randint(0, 4), 0)
-        for index3 in range(4):
-            if player == poison2:
-                player.delete()
-                game.game_over()
-            elif poison2.get(LedSpriteProperty.Y) == 4:
-                poison2.delete()
-                game.add_score(1)
-                poison2 = game.create_sprite(randint(0, 4), 0)
-            else:
-                basic.pause(500)
-                poison2.change(LedSpriteProperty.Y, 1)
+    global poison
+    for index in range(4):
+        if player == poison:
+            player.delete()
+            poison.delete()
+            basic.clear_screen()
+            game.game_over()
+        elif poison.get(LedSpriteProperty.Y) == 4:
+            poison.delete()
+            game.add_score(1)
+            poison = game.create_sprite(randint(0, 4), 0)
+        else:
+            basic.pause(500)
+            poison.change(LedSpriteProperty.Y, 1)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,60 +1,26 @@
-input.onGesture(Gesture.TiltLeft, function () {
+input.onButtonPressed(Button.A, function () {
     player.change(LedSpriteProperty.X, -1)
 })
-input.onGesture(Gesture.TiltRight, function () {
+input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, 1)
 })
-let poison2: game.LedSprite = null
-let _this = 0
 let player: game.LedSprite = null
 player = game.createSprite(2, 4)
 let poison = game.createSprite(randint(0, 4), 0)
 basic.forever(function () {
-    _this = randint(1, 2)
-    if (_this == 1) {
-        for (let index = 0; index < 4; index++) {
-            if (player == poison) {
-                player.delete()
-                poison.delete()
-                game.gameOver()
-            } else if (poison.get(LedSpriteProperty.Y) == 4) {
-                poison.delete()
-                game.addScore(1)
-                poison = game.createSprite(randint(0, 4), 0)
-            } else {
-                basic.pause(500)
-                poison.change(LedSpriteProperty.Y, 1)
-            }
-        }
-    } else {
-        for (let index = 0; index < 4; index++) {
-            if (player == poison) {
-                player.delete()
-                poison.delete()
-                poison2.delete()
-                game.gameOver()
-            } else if (poison.get(LedSpriteProperty.Y) == 4) {
-                poison.delete()
-                game.addScore(1)
-                poison = game.createSprite(randint(0, 4), 0)
-            } else {
-                basic.pause(500)
-                poison.change(LedSpriteProperty.Y, 1)
-            }
-        }
-        poison2 = game.createSprite(randint(0, 4), 0)
-        for (let index = 0; index < 4; index++) {
-            if (player == poison2) {
-                player.delete()
-                game.gameOver()
-            } else if (poison2.get(LedSpriteProperty.Y) == 4) {
-                poison2.delete()
-                game.addScore(1)
-                poison2 = game.createSprite(randint(0, 4), 0)
-            } else {
-                basic.pause(500)
-                poison2.change(LedSpriteProperty.Y, 1)
-            }
+    for (let index = 0; index < 4; index++) {
+        if (player == poison) {
+            player.delete()
+            poison.delete()
+            basic.clearScreen()
+            game.gameOver()
+        } else if (poison.get(LedSpriteProperty.Y) == 4) {
+            poison.delete()
+            game.addScore(1)
+            poison = game.createSprite(randint(0, 4), 0)
+        } else {
+            basic.pause(500)
+            poison.change(LedSpriteProperty.Y, 1)
         }
     }
 })
