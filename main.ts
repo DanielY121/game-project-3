6a351f7b7c63ca54e0a7fadb29e2eca8b6b797bc
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-04-08 15:58:34 +0800

    change logic

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, 1)
 })
+let sprite = 0
 let player: game.LedSprite = null
 player = game.createSprite(2, 4)
 let poison = game.createSprite(randint(0, 4), 0)
@@ -17,7 +18,12 @@ basic.forever(function () {
         } else if (poison.get(LedSpriteProperty.Y) == 4) {
             poison.delete()
             game.addScore(1)
-            poison = game.createSprite(randint(0, 4), 0)
+            sprite = randint(0, 10)
+            if (sprite > 0) {
+                poison = game.createSprite(randint(0, 4), 0)
+            } else {
+                poison = game.createSprite(player.get(LedSpriteProperty.X), 0)
+            }
         } else {
             basic.pause(500)
             poison.change(LedSpriteProperty.Y, 1)
